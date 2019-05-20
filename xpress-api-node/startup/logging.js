e511d8b2a96719ae0bc059f8e901ff2f07545441
D Polansky <zizuar@comcast.net>
2019-05-20 16:04:20 -0500

    Missed a file

--- xpress-api-node/startup/logging.js
+++ xpress-api-node/startup/logging.js
@@ -1,19 +1,15 @@
-const winston = require('winston');
-// require('winston-mongodb');
-require('express-async-errors');
+const winston = require("winston");
+require("express-async-errors");
 
 module.exports = function() {
   winston.handleExceptions(
     new winston.transports.Console({ colorize: true, prettyPrint: true }),
-    new winston.transports.File({ filename: 'uncaughtExceptions.log' }));
-  
-  process.on('unhandledRejection', (ex) => {
+    new winston.transports.File({ filename: "uncaughtExceptions.log" })
+  );
+
+  process.on("unhandledRejection", ex => {
     throw ex;
   });
-  
-  winston.add(winston.transports.File, { filename: 'logfile.log' });
-  // winston.add(winston.transports.MongoDB, { 
-  //   db: 'mongodb://localhost/vidly',
-  //   level: 'info'
-  // });  
-}
+
+  winston.add(winston.transports.File, { filename: "logfile.log" });
+};
